2b44a813a64192d99dfdfce2e1c8fa7fdc852574
Bruno Henrique <brunohbm23@gmail.com>
2022-07-23 20:43:43 -0300

    Creation of the project and rendering of all the the notes as buttons.

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  // Most tests are like this
+  expect(true).toBe(true);
+});
